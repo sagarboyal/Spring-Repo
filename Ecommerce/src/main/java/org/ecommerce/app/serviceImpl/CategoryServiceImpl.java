cc3721e3b361a1cb7e84fd83b178086f8e338c32
sagarboyal <sboyal12@gmail.com>
2025-01-31 21:01:14 +0530

    Making Category persistent!

--- Ecommerce/src/main/java/org/ecommerce/app/serviceImpl/CategoryServiceImpl.java
+++ Ecommerce/src/main/java/org/ecommerce/app/serviceImpl/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package org.ecommerce.app.serviceImpl;
 
 import org.ecommerce.app.model.Category;
+import org.ecommerce.app.repository.CategoryRepository;
 import org.ecommerce.app.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,16 +15,24 @@ import java.util.stream.IntStream;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private final List<Category> categoryList = new ArrayList<Category>();
-    private long countCategory = 1;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    private long countCategory = 1L;
 
     @Override
     public List<Category> getCategoryList() {
-        return categoryList;
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void addCategory(Category category) {
+        countCategory++;
+        categoryRepository.save(category);
     }
 
     @Override
     public Category getCategoryById(long id) {
+        List<Category> categoryList = categoryRepository.findAll();
         return categoryList.stream()
                 .filter(category ->
                         category.getCategoryId()
@@ -32,27 +42,19 @@ public class CategoryServiceImpl implements CategoryService {
                             new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
     }
 
-    @Override
-    public void addCategory(Category category) {
-        category.setCategoryId(countCategory++);
-        categoryList.add(category);
-    }
+
 
     @Override
     public void deleteCategoryById(long id) {
         Category category = getCategoryById(id);
         if (category != null)
-            categoryList.remove(category);
+            categoryRepository.delete(category);
     }
 
     @Override
     public void updateCategory(Category category) {
-        int index = IntStream.range(0, categoryList.size())
-                .filter(i -> categoryList.get(i).getCategoryId().equals(category.getCategoryId()))
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
-        if (index != -1)
-            categoryList.set(index, category);
+        getCategoryById(category.getCategoryId());
+        categoryRepository.save(category);
     }
 
 }
