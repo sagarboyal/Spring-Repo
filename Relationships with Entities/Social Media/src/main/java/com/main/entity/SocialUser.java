4d336ef94a07f59e81086a6f7549052036313d45
sagarboyal <sboyal12@gmail.com>
2025-02-10 19:23:28 +0530

    One to One!

---
+++ Relationships with Entities/Social Media/src/main/java/com/main/entity/SocialUser.java
@@ -0,0 +1,13 @@
+package com.main.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class SocialUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
