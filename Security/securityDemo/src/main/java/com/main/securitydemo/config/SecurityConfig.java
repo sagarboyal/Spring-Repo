20f40307460082b0272b0bb4fe9e4a952a1041f2
sagarboyal <sboyal12@gmail.com>
2025-02-15 20:26:17 +0530

    Using Database to Store User Credentials with Spring Security!

--- Security/securityDemo/src/main/java/com/main/securitydemo/config/SecurityConfig.java
+++ Security/securityDemo/src/main/java/com/main/securitydemo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.main.securitydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,12 +14,18 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -47,7 +54,11 @@ public class SecurityConfig {
                 .password("{noop}1234")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+
+        return userDetailsManager;
     }
 
 }
