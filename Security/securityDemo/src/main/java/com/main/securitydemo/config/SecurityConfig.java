beefd75b06369db86a7a0c12263062d43eabcc61
sagarboyal <sboyal12@gmail.com>
2025-02-15 11:13:04 +0530

    own basic auth added!

---
+++ Security/securityDemo/src/main/java/com/main/securitydemo/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.main.securitydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
