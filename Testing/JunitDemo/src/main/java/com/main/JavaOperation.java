4da7762520b040ec590963ec7160e5b39567b806
sagarboyal <sboyal12@gmail.com>
2025-02-15 19:54:16 +0530

    created some basic test cases!

--- Testing/JunitDemo/src/main/java/com/main/JavaOperation.java
+++ Testing/JunitDemo/src/main/java/com/main/JavaOperation.java
@@ -4,4 +4,7 @@ public class JavaOperation {
     public int add(int a, int b) {
         return a + b;
     }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 }
--- Testing/JunitDemo/src/test/java/com/main/JavaOperationTest.java
+++ Testing/JunitDemo/src/test/java/com/main/JavaOperationTest.java
@@ -1,7 +1,16 @@
 package com.main;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaOperationTest {
-
+    @Test
+    void addTest(){
+        System.out.println("addTest");
+    }
+    @Test
+    void multiplyTest(){
+        System.out.println("multiplyTest");
+    }
 }
