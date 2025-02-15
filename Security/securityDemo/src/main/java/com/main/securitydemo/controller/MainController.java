91d8bebc109eb6d82cd664ce9bc3dcc712559b7a
sagarboyal <sboyal12@gmail.com>
2025-02-15 12:13:33 +0530

    role base auth added!

--- Security/securityDemo/src/main/java/com/main/securitydemo/controller/MainController.java
+++ Security/securityDemo/src/main/java/com/main/securitydemo/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.main.securitydemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,15 @@ public class MainController {
     @GetMapping
     public String getHome(){
         return "Hello World";
+    }
+    @PreAuthorize("hasRole('USER')") // for this we need to active method security in config
+    @GetMapping("/user")
+    public String getHomeUser(){
+        return "User Home";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String getHomeAdmin(){
+        return "Admin Home";
     }
 }
