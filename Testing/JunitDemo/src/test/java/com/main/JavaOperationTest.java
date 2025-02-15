51b5b3afecc86248a5676a6c12074cc0292c2bc9
sagarboyal <sboyal12@gmail.com>
2025-02-15 21:26:32 +0530

    BeforeEach and AfterEach!

--- Testing/JunitDemo/src/test/java/com/main/JavaOperationTest.java
+++ Testing/JunitDemo/src/test/java/com/main/JavaOperationTest.java
@@ -1,10 +1,24 @@
 package com.main;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class JavaOperationTest {
+
+    JavaOperation javaOperation;
+
+    @BeforeEach // allow you to call this method before calling every test case
+    public void init(){
+        javaOperation = new JavaOperation();
+        System.out.println("JavaOperation init");
+    }
+    @AfterEach // allow you to call this method after calling every test case
+    public void cleanUp(){
+        System.out.println("cleanUp done");
+    }
+
     @Test
     void addTest(){
         System.out.println("addTest");
