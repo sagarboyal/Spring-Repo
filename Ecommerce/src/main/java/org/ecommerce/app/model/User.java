4a7d7d43e28054c3047b135354f0ab2a23509f1a
sagarboyal <sboyal12@gmail.com>
2025-02-17 11:40:22 +0530

    seller side relationship added!

--- Ecommerce/src/main/java/org/ecommerce/app/model/User.java
+++ Ecommerce/src/main/java/org/ecommerce/app/model/User.java
@@ -54,4 +54,9 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles;
+
+    @OneToMany(mappedBy = "user",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            orphanRemoval = true)
+    private Set<Product> products;
 }
