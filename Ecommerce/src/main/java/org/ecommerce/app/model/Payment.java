754993b0ec7c1010ce47728310a3369521825918
sagarboyal <sboyal12@gmail.com>
2025-03-06 23:02:18 +0530

    Order and Payment model added!

---
+++ Ecommerce/src/main/java/org/ecommerce/app/model/Payment.java
@@ -0,0 +1,40 @@
+package org.ecommerce.app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @OneToOne(mappedBy = "payment", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Order order;
+
+    @NotBlank
+    @Size(min = 4, message = "Payment method must contain at least 4 characters")
+    private String paymentMethod;
+
+
+    private String paymentGatewayId;
+    private String paymentGatewayName;
+    private String paymentGatewayStatus;
+    private String paymentGatewayResponseMessage;
+
+    public Payment(Long paymentId, String paymentGatewayId, String paymentGatewayName, String paymentGatewayStatus, String paymentGatewayResponseMessage) {
+        this.paymentId = paymentId;
+        this.paymentGatewayId = paymentGatewayId;
+        this.paymentGatewayName = paymentGatewayName;
+        this.paymentGatewayStatus = paymentGatewayStatus;
+        this.paymentGatewayResponseMessage = paymentGatewayResponseMessage;
+    }
+}
