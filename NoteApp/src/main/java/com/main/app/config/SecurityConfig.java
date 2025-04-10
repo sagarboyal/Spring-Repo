e263f6f135e14c9fd2d26eb82c4775764eee5bad
sagarboyal <sboyal12@gmail.com>
2025-04-10 17:10:25 +0530

    Converted InMemoryAuth to JdbcUserDetailsAuth!

--- NoteApp/src/main/java/com/main/app/config/SecurityConfig.java
+++ NoteApp/src/main/java/com/main/app/config/SecurityConfig.java
@@ -8,8 +8,10 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -30,9 +32,9 @@ public class SecurityConfig {
         return http.build();
     }
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager =
-                new InMemoryUserDetailsManager();
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager manager =
+                new JdbcUserDetailsManager(dataSource);
         if (!manager.userExists("user1")) {
             manager.createUser(
                     User.withUsername("user1")
