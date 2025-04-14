00ae8c82499202bba3745e345ea0ef7d6db66273
sagarboyal <sboyal12@gmail.com>
2025-04-14 20:26:09 +0530

    Added a custom filter!

--- NoteApp/src/main/java/com/main/app/security/SecurityConfig.java
+++ NoteApp/src/main/java/com/main/app/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.main.app.security;
 
+import com.main.app.security.filters.CustomLoggingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,8 +27,13 @@ public class SecurityConfig {
                         .requestMatchers("api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/notes/**", "/api/notes").permitAll()
                         .anyRequest().authenticated());
+
+        http.addFilterBefore(new CustomLoggingFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+
         http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         http.httpBasic(withDefaults());
         return http.build();
     }
