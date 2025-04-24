681a013347ec95a1afc9d867600f1a4393dca513
sagarboyal <sboyal12@gmail.com>
2025-04-24 14:14:24 +0530

    Configuring SecurityConfig for jwt

--- NoteApp/src/main/java/com/main/app/security/SecurityConfig.java
+++ NoteApp/src/main/java/com/main/app/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.main.app.security;
 
+import com.main.app.jwt.AuthEntryPoint;
+import com.main.app.jwt.AuthTokenFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,13 +12,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthEntryPoint unauthorizedHandler;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -29,10 +37,20 @@ public class SecurityConfig {
                             .requestMatchers(
                                         "/api/notes/**",
                                         "/api/notes",
-                                        "api/csrf-token") .permitAll()
+                                        "api/csrf-token",
+                                        "api/auth/public/**") .permitAll()
                             .anyRequest().authenticated())
+                    .exceptionHandling(exception ->
+                                    exception.authenticationEntryPoint(unauthorizedHandler))
+                    .addFilterBefore(authTokenFilter(),
+                            UsernamePasswordAuthenticationFilter.class)
                     .httpBasic(withDefaults())
                     .formLogin(withDefaults())
                     .build();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
     }
 }
