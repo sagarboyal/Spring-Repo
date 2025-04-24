00219ee35469b93835d23d9e0430493bc55304aa
sagarboyal <sboyal12@gmail.com>
2025-04-24 08:03:29 +0530

    CSRF Protection enabled!

--- NoteApp/src/main/java/com/main/app/security/SecurityConfig.java
+++ NoteApp/src/main/java/com/main/app/security/SecurityConfig.java
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,19 +16,22 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) ->
-                requests
-                        .requestMatchers("api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/notes/**", "/api/notes").permitAll()
-                        .anyRequest().authenticated());
-
-        http.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http.httpBasic(withDefaults());
-        return http.build();
+        return http
+                    .csrf(csrf ->
+                            csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                                    .ignoringRequestMatchers("api/auth/public/**")
+                    )
+                    .authorizeHttpRequests((requests) ->
+                    requests
+                            .requestMatchers("api/admin/**").hasRole("ADMIN")
+                            .requestMatchers(
+                                        "/api/notes/**",
+                                        "/api/notes",
+                                        "api/csrf-token") .permitAll()
+                            .anyRequest().authenticated())
+                    .httpBasic(withDefaults())
+                    .formLogin(withDefaults())
+                    .build();
     }
 
     @Bean
