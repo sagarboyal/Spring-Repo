e9c880504f90d90dbc116c3747eab91ac2124f26
sagarboyal <sboyal12@gmail.com>
2025-04-24 13:57:33 +0530

    login req and response, controller added!

--- NoteApp/src/main/java/com/main/app/security/SecurityConfig.java
+++ NoteApp/src/main/java/com/main/app/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.main.app.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -38,4 +40,9 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 }
